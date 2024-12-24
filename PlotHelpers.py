760d6616b9dcef7e857238888669a4f234247ca4
dw.wizard <dw.wizard@gmail.com>
2024-12-24 00:05:47 -0600

    Add an alternate bar plot function that works better in some cases.

--- PlotHelpers.py
+++ PlotHelpers.py
@@ -27,6 +27,30 @@ def showBarPlot(dataDict: dict, barGroupLabels):
 
 ################################################################################
 
+def showBarPlotAlt(dataDict: dict, barGroupLabels):
+   
+   numBarGroups = len(barGroupLabels)   
+   numCategories = len(dataDict.items())
+   totalBarWidth = 0.8
+   barOffset = (1.0-totalBarWidth)/2.0
+   barWidth = totalBarWidth / float(numCategories)
+
+   fig = plt.subplots()
+   i = 0
+   for key, val in dataDict.items():
+      barPositions = [barOffset + x + barWidth*float(i) for x in np.arange(numBarGroups) ]
+      plt.bar(barPositions, val, width = barWidth, label = key) 
+      i += 1
+
+   labelOffset = (totalBarWidth-barOffset)/2#-barWidth/2 #barWidth * float((numCategories - 1)/2)
+   plt.xticks([r + labelOffset for r in range(numBarGroups)], barGroupLabels)
+
+   plt.legend()
+   plt.grid(axis='y')
+   plt.show()
+
+################################################################################
+
 def showStackedBarPlot(dataDict: dict, barGroupLabels):
    
    numBarGroups = len(barGroupLabels)   
