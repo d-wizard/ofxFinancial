056f91a3285c36bee6176664e4474f0797392ba4
dw.wizard <dw.wizard@gmail.com>
2025-11-09 19:41:18 -0600

    Make sure plot colors don't repeat.

--- PlotHelpers.py
+++ PlotHelpers.py
@@ -1,5 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import colorsys
+
+class betterColors:
+   goldenAngle = 137.5 / 360 # normalize
+
+   def __init__(self, h=0.1, s=1, v=0.98):
+      self._h = h
+      self._s = s
+      self._v = v
+
+   def get_next_color(self, ):
+      retVal = self.hsv_to_hex_rgb(self._h, self._s, self._v)
+      self._h += self.goldenAngle
+      self._s += 0.238461981
+      self._v += 0.064984
+      if self._s > 1:
+         self._s -= 0.598461
+      if self._v > 1:
+         self._v -= 0.2351647
+      print(retVal)
+      return retVal
+
+   def hsv_to_hex_rgb(self, h, s, v):
+      """
+      Converts HSV values (0-1 range) to a hexadecimal RGB string (#RRGGBB).
+
+      Args:
+         h (float): Hue, in the range [0, 1].
+         s (float): Saturation, in the range [0, 1].
+         v (float): Value, in the range [0, 1].
+
+      Returns:
+         str: A string representing the color in hexadecimal RGB format (e.g., "#FF0000").
+      """
+      # Convert HSV (0-1) to RGB (0-1)
+      r, g, b = colorsys.hsv_to_rgb(h, s, v)
+
+      # Scale RGB values to 0-255 and convert to integers
+      r_int = int(r * 255)
+      g_int = int(g * 255)
+      b_int = int(b * 255)
+
+      # Format as a hexadecimal string
+      hex_rgb = f"#{r_int:02X}{g_int:02X}{b_int:02X}"
+      return hex_rgb
 
 ################################################################################
 ################################################################################
@@ -60,8 +105,9 @@ def showStackedBarPlot(dataDict: dict, barGroupLabels):
 
    fig, ax = plt.subplots()
    bottom = [0] * numBarGroups
+   colors = betterColors()
    for key, val in dataDict.items():
-      plt.bar(barPositions, val, bottom = bottom, width = barWidth, label = key)
+      plt.bar(barPositions, val, bottom = bottom, width = barWidth, label = key, color=colors.get_next_color())
       for i in range(len(val)):
          bottom[i] += val[i]
 
